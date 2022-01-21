e4fa7593ec8de24b84d54462c29e8f9cb5058cb2
minz <ghdalswl77@naver.com>
2022-01-21 15:41:20 +0900

    금요일

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Nav/Header";
+import Props from "./Props_1.js";
+
+function Main() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Main;
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  mainBlue: "#00c7f5",
+  backgroundGrey: "#f5f5f5",
+  // border color
+  borderLine: "#ededed",
+  inputGray: "#dbdbdb",
+  // font color
+  fontMainBlack: "#424242",
+  fontNavBlack: "#474747",
+  fontSubNavBlack: "#424242",
+  fontFilterGray: "#757575",
+  fontGray: "#757575",
+  placeholderGrey: "#bdbdbd",
+};
+
+export default theme;
